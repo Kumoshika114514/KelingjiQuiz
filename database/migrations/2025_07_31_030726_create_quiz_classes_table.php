ef5a8cb0b4f36ac778d20d230e56038a3a020775
Kumoshika114514 <chanhuanlin2343@gmail.com>
2025-09-01 20:38:28 +0800

    Repair migration

--- database/migrations/2025_07_31_030726_create_quiz_classes_table.php
+++ database/migrations/2025_07_31_030726_create_quiz_classes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('quiz_classes', function (Blueprint $table) {
+        Schema::create('quiz_classes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('class_code')->unique();
